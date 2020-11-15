2d0e89abecb5c85f313a2349d816e7faaaac0b9e
aspunds <jonathan@asplund.net>
2020-11-15 20:48:49 +0100

    Initial commit

---
+++ Tappify.js
@@ -0,0 +1,5 @@
+import Cursor from "./lib/Cursor";
+import Finger from "./lib/Finger";
+import { cursor as isCursor, subscribe } from "./lib/PointerBase";
+
+export default { Cursor, Finger, isCursor, subscribe }
